395b46e364630f7050d3ab1494e6511414fc406d
Dvin <dvyu@yandex.ru>
2020-10-26 21:07:53 +0500

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,10 +21,11 @@ ssh-keyscan github.com | ssh-keygen -lf - > ~/.ssh/known_hosts
 #grep "^github.com " ~/.ssh/known_hosts
 #ssh-agent bash -c "ssh-add /tmp/key; git clone ${GIT_SSH_REPOSITRY} ./GIT_SSH_REPOSITRY";
 cat ~/.ssh/known_hosts
-cat ~/.ssh/id_rsa
+#cat ~/.ssh/id_rsa
 export GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
-ssh-agent bash -c "ssh-add ~/.ssh/id_rsa";
-git clone ${GIT_SSH_REPOSITRY} ./GIT_SSH_REPOSITRY
+#ssh-agent bash -c "ssh-add ~/.ssh/id_rsa";
+ssh-agent bash -c "ssh-add ~/.ssh/id_rsa; git clone ${GIT_SSH_REPOSITRY} ./GIT_SSH_REPOSITRY";
+#git clone ${GIT_SSH_REPOSITRY} ./GIT_SSH_REPOSITRY
 cd ./GIT_SSH_REPOSITRY
 echo YAML_FILE=${YAML_FILE}
 grep -i image: ${YAML_FILE}
